4acb78d69605a73e080763351c9bb3a702c98854
James Ortiz <jamesaortiz75@gmail.com>
2019-08-15 23:55:19 -0400

    Created initial word analyzer

---
+++ wordAnalyzer.py
@@ -0,0 +1,40 @@
+#
+# Author:  James Ortiz
+# File:    pyWordScraper.py
+# Purpose: This program is designed to read in a file,
+# create a list of lists, and then provide analytical
+# information 
+#
+
+# function lexicographicalSort(str)
+# Python function to sort the words in lexicographical 
+# order   
+def lexicographicalSort(my_string): 
+    # Split the my_string till where space is found. 
+    words = my_string.split() 
+    # sort() will sort the strings. 
+    words.sort() 
+    # Iterate i through 'words' to print the words 
+    # in alphabetical manner. 
+    #for i in words: 
+    #    print(i)
+    return words
+
+
+def main():
+    file = open('episodeIVCrawl.txt', 'r')
+    text = file.read().strip()
+    file.close()
+
+    newString = lexicographicalSort(text)
+
+    s = " "
+    s = s.join(newString)
+    s = s.replace(' ', '\n')
+    
+    
+    newFile = open('output.txt', 'w')
+    newFile.write(s)
+    
+if __name__ == "__main__":
+    main()
